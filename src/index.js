d97f225b671a98f4a80c7bc5b1fa2f623f3c1843
soumyajeet <sendtosoumyajit@gmail.com>
2022-05-24 14:00:14 +0530

    amplify added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 
 ReactDOM.render(
