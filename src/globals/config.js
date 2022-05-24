8eeda644811b01b2a8ed383f4b40db8af41668da
soumyajeet <sendtosoumyajit@gmail.com>
2022-05-24 20:39:20 +0530

    changed config

--- src/globals/config.js
+++ src/globals/config.js
@@ -1,2 +1,2 @@
-export const API_URL = `https://okupm1rboi.execute-api.ap-south-1.amazonaws.com/master`
-//export const API_URL = `http://localhost:8083/api`;
+//export const API_URL = `https://okupm1rboi.execute-api.ap-south-1.amazonaws.com/master`
+export const API_URL = `http://localhost:8083/api`;
